6b7b21031fb5a83d4c0c91dfa4226f3d53c79cb4
Roman <smyrnovroman@gmail.com>
2013-12-16 22:09:35 +0000

    Finished questions

--- app.js
+++ app.js
@@ -5,24 +5,58 @@
 
 var express = require('express');
 var routes = require('./routes');
+var questions = require('./routes/questions');
 var http = require('http');
+var orm = require('orm');
+var sqlite3 = require('sqlite3').verbose();
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(app.router);
+app.use(express.bodyParser());
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.use(orm.express("sqlite://ws.db", {
+    define: function (db, models, next) {
+        models.question = db.define("question", { 
+        	title: {type: "text", size: 100, required: true},
+        	body: {type: "text", size: 1000, required: true},
+        	date: {type: "date", time: true, required: true}
+        });
+        models.answer = db.define("answer", { 
+        	body: {type: "text", size: 1000, required: true},
+        	date: {type: "date", time: true, required: true}
+        });
+        models.comment = db.define("comment", { 
+        	body: {type: "text", size: 1000, required: true},
+        	date: {type: "date", time: true, required: true}
+        });
+        models.question.hasMany("answers", models.answer, {}, { reverse: "questions"});
+        models.question.hasMany("comments", models.comment, {}, { reverse: "questions"});
+        models.answer.hasMany("comments", models.comment, {}, { reverse: "answers"});
+        db.sync(function(err) {
+        	if (err) {
+        		throw err;
+        	}
+            next();
+        });
+    }
+}));
+
+app.get("/", routes.index);
+app.post("/questions", questions.create_question);
+app.get("/questions", questions.get_questions);
+app.get("/questions/:id", questions.get_question);
+app.del("/questions/:id", questions.delete_question);
+app.put("/questions/:id", questions.update_question);
+
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
--- routes/questions.js
+++ routes/questions.js
@@ -3,6 +3,8 @@
  * Creation, delete, update and retrieval of questions
  */
 
+var helper = require("../js/helper");
+
 exports.create_question = function(req, resp) {
 	var title = req.body.title;
 	var body = req.body.body;
@@ -23,3 +25,241 @@ exports.create_question = function(req, resp) {
 		resp.send(400);
 	}
 };
+
+exports.get_questions = function(req, resp) {
+	var count = req.query.count;
+	var rangeStart = req.query.start;
+	var rangeEnd = req.query.end;
+	var inputValid = true;
+	if (count !== undefined) {
+		if (!helper.check_int(count)) {
+			inputValid = false;
+		} else {
+			count = parseInt(count);
+			if (count <= 0) {
+				inputValid = false;
+			}
+		}
+	}
+	if (rangeStart !== undefined && rangeEnd !== undefined) {
+		if (!helper.check_int(rangeStart) || !helper.check_int(rangeEnd)) {
+			inputValid = false;
+		} else {
+			rangeStart = parseInt(rangeStart);
+			rangeEnd = parseInt(rangeEnd);
+			if (rangeStart < 0 || rangeStart > rangeEnd) {
+				inputValid = false;
+			}
+		}
+	} else if (rangeStart !== undefined || rangeEnd !== undefined) {
+		inputValid = false;
+	}
+	if (!inputValid) {
+		resp.send(400);
+	} else {
+		var offset = null, 
+			limit = null;
+		if (rangeStart !== undefined) {
+			offset = rangeStart;
+			limit = rangeEnd - rangeStart + 1;
+		} else if (count !== undefined) {
+			offset = 0;
+			limit = count;
+		};
+		var callback = function(err, questions) {
+	    	encode_questions(questions, function(err, json_questions) {
+	    		if (err) {
+	    			resp.send(err);
+	    		} else {
+	    			resp.send(json_questions);
+	    		}
+	    	});
+		};
+		if (offset !== null) {
+			req.models.question.find(null, ["date", "Z"])
+							    .limit(limit)
+							    .offset(offset)
+							    .run(callback);
+		} else {
+			req.models.question.find(null, ["date", "Z"])
+							   .run(callback);
+		}
+	}
+};
+
+exports.get_question = function(req, resp) {
+	find_question(req, req.params.id, function(err, question) {
+		if (err) {
+			resp.send(err);
+		} else {
+			encode_question(question, function(err, json_question) {
+				if (err) {
+					resp.send(err);
+				} else {
+					resp.send(json_question);
+				}
+			});
+		}
+	});
+};
+
+exports.delete_question = function(req, resp) {
+	find_question(req, req.params.id, function(err, question) {
+		if (err) {
+			resp.send(err);
+		} else {
+			question.getAnswers(function(err, answers) {
+				if (err) {
+					resp.send(500);
+				} else {
+					remove_answers(answers, function(err) {
+						if (err) {
+							resp.send(err);
+						} else {
+							remove_comments(question, function(err) {
+								if (err) {
+									resp.send(500);
+								} else {
+									question.remove(function(err) {
+										if (err) {
+											resp.send(500);
+										} else {
+											resp.send(204);
+										}
+									});
+								}
+							});
+						}
+					});
+				}
+			});
+		}	
+	}); 
+};
+
+exports.update_question = function(req, resp) {
+	find_question(req, req.params.id, function(err, question) {
+		if (err) {
+			resp.send(err);
+		} else {
+			var title = req.body.title;
+			var body = req.body.body;
+			if (title) {
+				question.title = title;
+			}
+			if (body) {
+				question.body = body;
+			}
+			question.save(function(err) {
+				if (err) {
+					resp.send(500);
+				} else {
+					resp.send(204);
+				}
+			});
+		}
+	});
+};
+
+function find_question(req, id, callback) {
+	if (helper.check_int(id)) {
+		req.models.question.find({id : parseInt(id)}, function(err, questions) {
+			if (err) {
+				callback(500, null);
+			} else if (questions.length === 0) {
+				callback(404, null);
+			} else {
+				var question = questions[0];
+				callback(null, question);
+			}
+		});
+	} else {
+		callback.send(400);
+	}
+}
+
+function encode_questions(questions, callback, json_questions) {
+	if (!json_questions) {
+		json_questions = [];
+	}
+	if (questions.length === 0) {
+		callback(null, json_questions);
+	} else {
+		var question = questions.shift();
+		encode_question(question, function(err, json_question) {
+			if (err) {
+				callback(err, null);
+			} else {
+				json_questions.push(json_question);
+				encode_questions(questions, callback, json_questions);
+			}
+		});
+	}
+}
+
+function encode_question(question, callback) {
+	question.getAnswers(function(err, answers) {
+		if (err) {
+			callback(500, null);
+		} else {
+			question.getComments(function(err, comments) {
+				if (err) {
+					callback(500, null);
+				} else {
+					callback(null, {title: question.title,
+							  		body: question.body,
+							  		date: question.date,
+							  		answers_count: answers.length,
+							  		comments_count: comments.length});
+				}
+			});
+		}
+	});
+}
+
+function remove_answers(answers, callback) {
+	if (answers.length === 0) {
+		callback(null);
+	} else {
+		var answer = answers.shift();
+		remove_answer(answer, function(err) {
+			if (err) {
+				callback(err);
+			} else {
+				remove_answers(answers, callback);
+			}
+		});
+	}
+}
+
+function remove_answer(answer, callback) {
+	remove_comments(answer, function(err) {
+		if (err) {
+			callback(err);
+		} else {
+			answer.remove(function(err) {
+				if (err) {
+					callback(500);
+				} else {
+					callback(null);
+				}
+			});
+		}
+	});
+}
+
+function remove_comments(object, callback) {
+	object.getComments(function(err, comments) {
+		if (err) {
+			callback(500);
+		} else {
+			object.removeComments(comments, function(err) {
+				if (err) {
+					callback(500);
+				} else {
+					callback(null);
+				}
+			});
+		}
+	});
+}
